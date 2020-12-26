6b61a8e723c041536de8fb555896146190c3ce55
Gavin Vaske <42784674+gavinvaske@users.noreply.github.com>
2020-12-26 11:20:34 -0600

    Expand Lint Rules (#5)
    * Expand eslint rules; add new npm commands
    
    * fix eslint issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,15 @@ module.exports = {
         'ecmaVersion': 12
     },
     'rules': {
-      'semi': ['error', 'always'],
-      'quotes': ['error', 'single']
+        'semi': ['error', 'always'],
+        'quotes': ['error', 'single'],
+        'complexity': ['error', {'max': 6 }],
+        'eqeqeq': ['error', 'always', {'null': 'ignore'}],
+        'no-magic-numbers': ['error', {
+            'ignore': [1], 
+            'ignoreArrayIndexes': true 
+        }],
+        'no-multi-spaces': 'error',
+        'no-unused-vars': 'error'
     }
 };
