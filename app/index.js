a79658f670fbdffb5269049f5c682f49b5db8103
Gavin Vaske <Gavin.Vaske@gmail.com>
2021-01-03 14:48:38 -0600

    update login/register routing

--- app/index.js
+++ app/index.js
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
 app.use(express.static(__dirname + '/public'));
-app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 app.use(expressLayouts);
 
 app.use('/', require('./routes/index'));
--- app/routes/users.js
+++ app/routes/users.js
@@ -10,10 +10,16 @@ router.post('/', async (request, response) => {
         const saltComplexity = 10;
         const hashedPassword = await bcrypt.hash(request.body.password, saltComplexity);
         const userParameters = {
-            userName: request.body.userName,
+            email: request.body.email,
             password: hashedPassword
         };
         const newUser = new User(userParameters);
+
+        const userExists = User.findOne({email: request.body.email});
+
+        if (userExists) {
+            return response.send('Email already exists');
+        }
 
         newUser.save((error, document) => {
             if (error) {
@@ -50,8 +56,77 @@ router.delete('/:id', (request, response) => {
     });
 });
 
+router.get('/login', (request, response) => {
+    response.render('login.ejs');
+});
+
 router.get('/register', (request, response) => {
-    response.render('register');
+    response.render('register.ejs');
+});
+
+router.post('/register', async (request, response) => {
+    try {
+        const saltComplexity = 10;
+        const hashedPassword = await bcrypt.hash(request.body.password, saltComplexity);
+        const userParameters = {
+            name: request.body.name.toLowerCase(),
+            email: request.body.email.toLowerCase(),
+            password: hashedPassword
+        };
+        const newUser = new User(userParameters);
+
+        // eslint-disable-next-line no-magic-numbers
+        const doesUserExist = User.count({email: request.body.email}) > 0;
+
+        if (doesUserExist) {
+            return response.render('register', {
+                error: 'email already exists'
+            });
+        }
+
+        newUser.save((error) => {
+            if (error) {
+                response.render('register');
+            }
+            response.render('login');
+        });
+    } catch (error) {
+        console.log(`Error creating user: ${error}`);
+        response.render('register');
+    }
+});
+
+router.post('/login', async (request, response) => {
+    console.log(`request => ${JSON.stringify(request.body)} | ${JSON.stringify(request.params)}`);
+
+    const options = {};
+    const query = {email: request.body.email.toLowerCase()};
+    console.log(`query => ${JSON.stringify(query)}`);
+    const user = await User.findOne(query, options);
+
+    if (!user) {
+        response.render('login', {
+            error: 'Email does not exist'
+        });
+    }
+
+    try {
+        const isCorrectPassword = await bcrypt.compare(request.body.password, user.password);
+
+        if (isCorrectPassword) {
+            response.render('dashboard', {
+                user: user
+            });
+        } else {
+            response.render('login', {
+                error: 'Password is invalid'
+            });
+        }
+    } catch (error) {
+        response.render('login', {
+            error: 'Error occurred, please try again'
+        });
+    }
 });
 
 function handleError(error, response) {
